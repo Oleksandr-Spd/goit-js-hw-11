da70f8867aa8b75b7d891fc1ddf9f8e8d07d99b2
Oleksandr-Spd <133060544+Oleksandr-Spd@users.noreply.github.com>
2023-10-17 23:31:52 +0000

    Deploying to gh-pages from @ Oleksandr-Spd/goit-js-hw-11@5656adc27390336d850c856883deb1f57e50ce50 🚀

---
+++ index.a1475b82.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){$(window).scroll((function(){$(this).scrollTop()>300?$("a#move_up").fadeIn(200):$("a#move_up").fadeOut(400)})),$("a#move_up").click((function(){return $("body,html").animate({scrollTop:0},800),!1}))}));
+//# sourceMappingURL=index.a1475b82.js.map
